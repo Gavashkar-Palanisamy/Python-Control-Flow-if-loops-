5268553cb24ee18d53afbf7047844da36bbc0efa
Gavashkar Palanisamy <suriyagavash@gmail.com>
2025-10-23 21:59:02 +0530

    Add files via upload

---
+++ Largest of three numbers.py
@@ -0,0 +1,9 @@
+a=int(input('enter number:'))
+b=int(input('enter number:'))
+c=int(input('enter number:'))
+if(a>b and a>c):
+    print('a is larger')
+if(b>a and b>c):
+    print('b is larger')
+else:
+    print('c is bigger')
