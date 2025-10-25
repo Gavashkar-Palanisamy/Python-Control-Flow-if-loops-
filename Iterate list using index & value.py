8a8a2817ddf35681d4c29599d9e3d8b934b5a179
Gavashkar Palanisamy <suriyagavash@gmail.com>
2025-10-25 14:48:58 +0530

    Add files via upload

---
+++ Iterate list using index & value.py
@@ -0,0 +1,10 @@
+
+
+lst=[1,2,3,4,5,6,7]
+for index, value in enumerate(lst):
+    print('index :', index, 'value :', value)
+
+print('-------------------------------')
+
+lst=['suriya', 2, 6, 'yazhi']
+print(list(enumerate(lst)))
